1d1c9ebc2511b5e81fe6367bbe52fb9dcf2b2dbd
Tami Zwick <tami.zwick@yahoo.com>
2020-02-25 21:33:26 -0600

    enhances styling on admin screen

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import * as utility from '../../utility/utility';
+import classes from './Admin.module.css';
 import Button from '../UI/Button/Button';
 
 const admin = (props) => {
     return (
         <main className='main'>
-            <Button
-                btnClass='btn-secondary'
-                clicked={(event) => utility.pushHistory('/admin/new-user', props.history)}>
-                Add New User
-            </Button>
-            <Button
-                btnClass='btn-secondary'
-                clicked={(event) => utility.pushHistory('/admin/change-password', props.history)}>
-                Change My Password
-            </Button>
+            <div className={classes.btnContainer}>
+                <h2 className={classes.containerHeader}>User Management</h2>
+                <Button
+                    btnClass='btn-secondary'
+                    clicked={(event) => utility.pushHistory('/admin/new-user', props.history)}>
+                    Add New User
+                </Button>
+                <Button
+                    btnClass='btn-secondary'
+                    clicked={(event) => utility.pushHistory('/admin/change-password', props.history)}>
+                    Change My Password
+                </Button>
+            </div>
         </main>
     );
 }
