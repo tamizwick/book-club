6bbe91ec6814f78ec16a18e7c4ea7aa81af5f575
Tami Zwick <tzwick06@gmail.com>
2021-09-11 17:27:10 -0500

    updated carousel so when item is clicked user is taken to book details page

--- src/components/Books/BookCover/BookCover.js
+++ src/components/Books/BookCover/BookCover.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import classes from './BookCover.module.css';
+import * as utility from '../../../utility/utility';
+import button from '../../UI/Button/Button';
 
-const bookCover = (props) => {
+const BookCover = (props) => {
+    const history = useHistory();
+    const carouselItemClicked = () => {
+        utility.pushHistory(`/books/details/${props.book.isbn}`, history);
+        console.log(props.book.isbn + ' ' + props.book.title)
+    }
+    
     return (
-        <figure className={classes.figure}>
-            <img
-                key={props.book.isbn}
-                src={props.book.imageLink}
-                alt={`${props.book.title} cover`}
-                className={classes.cover} />
-            <figcaption>{props.book.title} by {props.book.author}</figcaption>
-        </figure>
+        <button className={classes.itemButton} onClick={carouselItemClicked}>
+            <figure className={classes.figure} >
+                <img
+                    key={props.book.isbn}
+                    src={props.book.imageLink}
+                    alt={`${props.book.title} cover`}
+                    className={classes.cover} />
+                <figcaption>{props.book.title} by {props.book.author}</figcaption>
+            </figure>
+        </button>
     );
 }
 
-export default bookCover;
+export default BookCover;
